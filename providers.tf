9bf8055d99420162b131a4fde742e7015c92cf33
Benjamin Smith <ben.smith.developer@gmail.com>
2021-06-21 15:11:07 -0700

    add vars for other modules to specify their aws provider arn

--- providers.tf
+++ providers.tf
@@ -4,3 +4,19 @@ provider "aws" {
       role_arn = var.assume_arn
     }
 }
+
+provider "aws" {
+  alias = "firewall_manager_admin"
+  region = var.region
+  assume_role {
+    role_arn = var.firewall_manager_administrator_arn
+  }
+}
+
+provider "aws" {
+  alias = "organization_management"
+  region = var.region
+  assume_role {
+    role_arn = var.organization_management_arn
+  }
+}
--- variables.tf
+++ variables.tf
@@ -54,3 +54,15 @@ variable "network_firewall_policies" {
   type    = list(any)
   default = []
 }
+
+variable "organization_management_arn" {
+  type        = string
+  description = "The ARN of the role you want to assume for applying these changes - must be a root account for setting up Firewall Manager Admin Account."
+  default     = null
+}
+
+variable "firewall_manager_administrator_arn" {
+  type        = string
+  description = "The ARN of the role you want to assume for destroying these changes - must be the AWS Firewall Manager administrator account. (contains admin_account_ids) "
+  default     = null
+}
