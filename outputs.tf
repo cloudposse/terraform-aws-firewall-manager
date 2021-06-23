f26898955a93360de977bd6610a8b3e077fb0d85
Benjamin Smith <ben.smith.developer@gmail.com>
2021-06-23 10:44:35 -0700

    cleanup

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-//output "admin_account_id" {
-//  description = "The AWS account ID of the AWS Firewall Manager administrator account."
-//  value       = join("", aws_fms_admin_account.default.*.id)
-//}
-
 output "aws_fms_account" {
   value = aws_fms_admin_account.default
 }
