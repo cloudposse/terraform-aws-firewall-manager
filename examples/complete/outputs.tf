e0f3a5b49ab86aeedfb39b77dc58af7321ece830
SweetOps <vladimir.fess@gmail.com>
2021-06-23 20:02:46 +0300

    upd

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,14 +1,4 @@
-output "id" {
-  description = "ID of the created example"
-  value       = module.example.id
-}
-
-output "example" {
-  description = "Output \"example\" from example module"
-  value       = module.example.example
-}
-
-output "random" {
-  description = "Output \"random\" from example module"
-  value       = module.example.random
+output "admin_account_id" {
+  description = "The AWS account ID of the AWS Firewall Manager administrator account."
+  value       = module.fms.admin_account_id
 }
--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 locals {
   enabled                                = module.this.enabled
-  admin_account_ids                      = local.enabled && length(var.admin_account_ids) > 0 ? var.admin_account_ids : [join("", data.aws_caller_identity.default.*.account_id)]
   security_groups_common_policies        = local.enabled && length(var.security_groups_common_policies) > 0 ? { for policy in flatten(var.security_groups_common_policies) : policy.name => policy } : {}
   security_groups_content_audit_policies = local.enabled && length(var.security_groups_content_audit_policies) > 0 ? { for policy in flatten(var.security_groups_content_audit_policies) : policy.name => policy } : {}
   security_groups_usage_audit_policies   = local.enabled && length(var.security_groups_usage_audit_policies) > 0 ? { for policy in flatten(var.security_groups_usage_audit_policies) : policy.name => policy } : {}
@@ -11,12 +10,8 @@ locals {
   network_firewall_policies              = local.enabled && length(var.network_firewall_policies) > 0 ? { for policy in flatten(var.network_firewall_policies) : policy.name => policy } : {}
 }
 
-data "aws_caller_identity" "default" {
-  count = local.enabled ? 1 : 0
-}
-
 resource "aws_fms_admin_account" "default" {
-  for_each = toset(local.admin_account_ids)
+  count = local.enabled ? 1 : 0
 
-  account_id = each.value
+  account_id = var.admin_account_id
 }
