a462d02ab48fdb510dfe98382622985356807c51
Benjamin Smith <ben.smith.developer@gmail.com>
2021-06-28 10:46:15 -0700

    features/firehose support (#4)
    * feat: initial implementation
    
    * upd
    
    * Auto Format
    
    * upd
    
    * Auto Format
    
    * upd
    
    * Auto Format
    
    * upd
    
    * Auto Format
    
    * last one for today
    
    * Auto Format
    
    * upd
    
    * minor updates
    
    * Auto Format
    
    * Aws firewall manager allow destroy (dynamic Provider) (#3)
    
    * init
    
    * add firehose prefix
    
    * updates from pr review
    
    * comments
    
    * make init
    make github/init
    make readme
    
    * Auto Format
    
    * update bucket module
    
    * Auto Format
    
    * use iam policy document
    
    * Auto Format
    
    Co-authored-by: SweetOps <vladimir.fess@gmail.com>
    Co-authored-by: cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>

---
+++ firehose.tf
@@ -0,0 +1,58 @@
+module "firehose_label" {
+  source  = "cloudposse/label/null"
+  version = "0.24.1"
+
+  attributes = ["firehose"]
+
+  context = module.this.context
+}
+
+module "firehose_s3_bucket" {
+  count                  = local.enabled && var.firehose_enabled ? 1 : 0
+  source                 = "cloudposse/s3-bucket/aws"
+  version                = "0.38.0"
+  acl                    = "private"
+  enabled                = true
+  user_enabled           = true
+  versioning_enabled     = false
+  allowed_bucket_actions = ["s3:GetObject", "s3:ListBucket", "s3:GetBucketLocation"]
+  name                   = module.firehose_label.id
+  stage                  = module.this.stage
+  namespace              = module.this.namespace
+  bucket_name            = module.firehose_label.id
+
+  context = module.this.context
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  count = local.enabled ? 1 : 0
+
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["firehose.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "firehose_role" {
+  count = local.enabled && var.firehose_enabled ? 1 : 0
+  name  = module.firehose_label.id
+
+  assume_role_policy = join("", data.aws_iam_policy_document.assume_role.*.json)
+}
+
+resource "aws_kinesis_firehose_delivery_stream" "firehose_stream" {
+  count = local.enabled && var.firehose_enabled ? 1 : 0
+  // `aws-waf-logs-` required by AWS - https://aws.amazon.com/premiumsupport/knowledge-center/waf-configure-comprehensive-logging/
+  name        = format("%s%s", "aws-waf-logs-", module.this.id)
+  destination = "s3"
+
+  s3_configuration {
+    role_arn   = join("", aws_iam_role.firehose_role.*.arn)
+    bucket_arn = join("", module.firehose_s3_bucket.*.bucket_arn)
+  }
+}
