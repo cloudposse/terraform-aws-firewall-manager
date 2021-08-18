22f6ce62faf88cecfed87b9311926f5023f6ab52
Pavel <pavel.susloparov@vts.com>
2021-08-18 12:02:32 -0400

    :art: fix for include/exclude accounts for waf_v2 policies. Disable logging for firehose

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ locals {
   logging_config_firehose_arn     = { logDestinationConfigs : [var.firehose_arn], redactedFields : [{ redactedFieldType : "SingleHeader", redactedFieldValue : "Cookies" }, { redactedFieldType : "Method" }] }
   logging_config_firehose_enabled = { logDestinationConfigs : [join("", aws_kinesis_firehose_delivery_stream.firehose_stream.*.id)], redactedFields : [{ redactedFieldType : "SingleHeader", redactedFieldValue : "Cookies" }, { redactedFieldType : "Method" }] }
 
-  logging_configuration = local.enabled && var.firehose_enabled ? local.logging_config_firehose_enabled : local.enabled && var.firehose_arn != null ? local.logging_config_firehose_arn : {}
+  logging_configuration = {}
 }
 
 resource "aws_fms_admin_account" "default" {
