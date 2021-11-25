c108a0e2c6a9ad3bff600d7f6c0f632fc4809373
nitrocode <nitrocode@users.noreply.github.com>
2021-11-25 14:38:56 -0600

    Update outputs.tf

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,4 +1,4 @@
-output "firewall_manager_admin_account_id" {
-  value       = module.firewall_manager.admin_account.account_id
-  description = "AWS Account ID of the designated admin account."
+output "firewall_manager_admin_account" {
+  value       = module.firewall_manager.admin_account
+  description = "AWS info of the designated admin account."
 }
