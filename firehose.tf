aab0685a5d3efa2fc6b70d8339c02e408e237b91
Mikhail Shnaydman <mikhail.shnaydman@vts.com>
2022-04-08 13:35:51 -0400

    update lifecycle policy

--- firehose.tf
+++ firehose.tf
@@ -5,27 +5,9 @@ locals {
 
     abort_incomplete_multipart_upload_days = 1 # number
 
-    filter_and = null
     expiration = {
       days = 30 # integer > 0
     }
-    noncurrent_version_expiration = {
-      newer_noncurrent_versions = 3  # integer > 0
-      noncurrent_days           = 60 # integer >= 0
-    }
-    transition = [{
-      days          = 30            # integer >= 0
-      storage_class = "STANDARD_IA" # string/enum, one of GLACIER, STANDARD_IA, ONEZONE_IA, INTELLIGENT_TIERING, DEEP_ARCHIVE, GLACIER_IR.
-      },
-      {
-        days          = 60           # integer >= 0
-        storage_class = "ONEZONE_IA" # string/enum, one of GLACIER, STANDARD_IA, ONEZONE_IA, INTELLIGENT_TIERING, DEEP_ARCHIVE, GLACIER_IR.
-    }]
-    noncurrent_version_transition = [{
-      newer_noncurrent_versions = 3            # integer >= 0
-      noncurrent_days           = 30           # integer >= 0
-      storage_class             = "ONEZONE_IA" # string/enum, one of GLACIER, STANDARD_IA, ONEZONE_IA, INTELLIGENT_TIERING, DEEP_ARCHIVE, GLACIER_IR.
-    }]
   }]
 }
 
